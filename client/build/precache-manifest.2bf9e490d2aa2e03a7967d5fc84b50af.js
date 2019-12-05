90493888b7327a7046381482f9d479f6a70f6e36
Marcin Radecki <marcin.r@kloud9.nyc>
2019-12-05 11:08:48 +0100

    heroku

---
+++ client/build/precache-manifest.2bf9e490d2aa2e03a7967d5fc84b50af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305536449d4cef99a3fb07c513b7fe79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7674841fdd37b9005397",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bc97e312f9e50d5ddc32",
+    "url": "/static/js/2.4ae654f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4ae654f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7674841fdd37b9005397",
+    "url": "/static/js/main.bf4c47fa.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
