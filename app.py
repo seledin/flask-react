f76c3f1f2af3026d4f0ac455355854eb8796deb5
Marcin Radecki <marcin.r@kloud9.nyc>
2019-12-06 12:41:14 +0100

    data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from methods import keyword_forecaster_v1
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__,
@@ -31,7 +32,23 @@ def ping_pong():
         'message': 'pong!'
     })
 
+@app.route('/new', methods=['POST'])
+def echo_new():
+    keywords = request.json['keywords']
+    d = keyword_forecaster_v1(kw_list_in=keywords)
 
+    dictionary = {}
+
+    for keyword in keywords:
+        dictionary[keyword] = d[keyword].to_dict()
+        k = keyword + "F"
+        dictionary[k] = d[k].to_dict()
+
+
+
+    result = pd.DataFrame.from_dict(dictionary)
+
+    return json.loads(result.to_json(orient='columns', date_format='iso'))
 
 if __name__ == '__main__':
     app.run()
